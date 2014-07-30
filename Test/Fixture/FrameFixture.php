0889dd3744f0c31c5c5b25d17e6974e672ebe583
kteraguchi <kteraguchi@commonsnet.org>
2014-07-30 18:19:57 +0900

    Modified not to use import property

--- Test/Fixture/FrameFixture.php
+++ Test/Fixture/FrameFixture.php
@@ -15,11 +15,29 @@
 class FrameFixture extends CakeTestFixture {
 
 /**
- * Import
+ * Fields
  *
  * @var array
  */
-	public $import = array('model' => 'Frames.Frame');
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'room_id' => array('type' => 'integer', 'null' => false, 'default' => null),
+		'box_id' => array('type' => 'integer', 'null' => false, 'default' => null),
+		'plugin_id' => array('type' => 'integer', 'null' => false, 'default' => null),
+		'block_id' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'weight' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'is_published' => array('type' => 'boolean', 'null' => true, 'default' => null),
+		'from' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'to' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'created_user_id' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'modified_user_id' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
 
 /**
  * Records
